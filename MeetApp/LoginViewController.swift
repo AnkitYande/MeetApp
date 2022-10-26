f871571e497b5d84c38c235fbfa43eacf901a096
Bo <boyang.deng@gmail.com>
2022-10-25 22:44:56 -0500

    refactor login to use guard

--- MeetApp/LoginViewController.swift
+++ MeetApp/LoginViewController.swift
@@ -32,28 +32,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
-        if loginEmailTextField.hasText && loginPasswordTextField.hasText {
-            Auth.auth().signIn(withEmail: loginEmailTextField.text!, password: loginPasswordTextField.text!) { [weak self] authResult, error in
-                guard let strongSelf = self else { return }
-                guard let user = authResult?.user, error == nil else {
-                    let loginErrorAlert = UIAlertController(
-                        title: "Login Error",
-                        message: error!.localizedDescription,
-                        preferredStyle: .alert
-                    )
-                    let okButton = UIAlertAction(
-                        title: "Ok",
-                        style: .default
-                    )
-                    loginErrorAlert.addAction(okButton)
-                    strongSelf.present(loginErrorAlert, animated: true)
-                    return
-                }
-                print("login successful")
-                print("user uid: \(user.uid)")
-                strongSelf.performSegue(withIdentifier: "loginSegue", sender: strongSelf)
-            }
-        } else {
+        guard loginEmailTextField.hasText && loginPasswordTextField.hasText else {
             let incompleteAlert = UIAlertController(
                 title: "Incomplete Login",
                 message: "Please enter a valid email and password",
@@ -67,6 +46,27 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             
             incompleteAlert.addAction(okButton)
             present(incompleteAlert, animated: true)
+            return
+        }
+        Auth.auth().signIn(withEmail: loginEmailTextField.text!, password: loginPasswordTextField.text!) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+            guard let user = authResult?.user, error == nil else {
+                let loginErrorAlert = UIAlertController(
+                    title: "Login Error",
+                    message: error!.localizedDescription,
+                    preferredStyle: .alert
+                )
+                let okButton = UIAlertAction(
+                    title: "Ok",
+                    style: .default
+                )
+                loginErrorAlert.addAction(okButton)
+                strongSelf.present(loginErrorAlert, animated: true)
+                return
+            }
+            print("login successful")
+            print("user uid: \(user.uid)")
+            strongSelf.performSegue(withIdentifier: "loginSegue", sender: strongSelf)
         }
     }
     
