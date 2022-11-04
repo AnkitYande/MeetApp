2628219de6e5f173a5fa8e58ed9bee91817113ab
Ankit Yande <ankityande@gmail.com>
2022-11-04 14:14:56 -0500

    overhauled eventes and buttons

---
+++ MeetApp/ButtonControlView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonControlView.swift
+//  MeetApp
+//
+//  Created by Ankit Yande on 11/4/22.
+//
+
+import SwiftUI
+
+struct ButtonControlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonControlView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonControlView()
+    }
+}
---
+++ MeetApp/CommonUI.swift
@@ -0,0 +1,20 @@
+//
+//  CommonUI.swift
+//  MeetApp
+//
+//  Created by Ankit Yande on 11/4/22.
+//
+
+import SwiftUI
+
+struct CommonUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommonUI_Previews: PreviewProvider {
+    static var previews: some View {
+        CommonUI()
+    }
+}
