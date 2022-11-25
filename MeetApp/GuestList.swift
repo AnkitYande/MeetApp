63712c833e36d3c7cdc4fd81fab6afb9ef5faae4
Bo <boyang.deng@gmail.com>
2022-11-24 19:36:25 -0600

    add pictures to guest list

--- MeetApp/GuestList.swift
+++ MeetApp/GuestList.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 import FirebaseDatabase
+import FirebaseStorage
 
 struct GuestList: View {
     
     let userList:[String]
     @State var users:[User] = []
+    @State var usersProfilePictures:[UIImage] = []
 
     var body: some View {
         List {
-            ForEach(users, id:\.UID) { user in
+            ForEach(Array(users.enumerated()), id:\.offset) { index, user in
                 Text(user.displayName)
+                Image(uiImage: usersProfilePictures[index])
             }
         }
         .refreshable {
@@ -45,9 +48,31 @@ struct GuestList: View {
                 let latitude = userInfo["latitude"] as! Double
                 let longitude = userInfo["longitude"] as! Double
                 let newUser = User(UID: userID, email: email, displayName: displayName, username: username, profilePic: profilePic, status: status, latitude: latitude, longitude: longitude, eventsInvited: [], eventsHosting: [])
-                self.users.append(newUser)
+                
+                let imageRef = Storage.storage().reference(forURL: profilePic)
+                imageRef.getData(maxSize: 1 * 1024 * 1024, completion: { data, error in
+                    guard error == nil else {
+                        print(error!.localizedDescription)
+                        return;
+                    }
+                    let image = self.resizeImage(image: UIImage(data: data!)!, newWidth: 50, newHeight: 50)
+                    
+                    self.users.append(newUser)
+                    self.usersProfilePictures.append(image)
+                    
+                })
             })
         }
+    }
+    
+    private func resizeImage(image: UIImage, newWidth: CGFloat, newHeight: CGFloat) -> UIImage {
+        let scale = newWidth / image.size.width
+        //let newHeight = image.size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
     }
 }
 
